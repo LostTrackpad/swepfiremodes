9b899691143a06747dbe75b921462d281866f8d3
LostTrackpad <lamhung12700@gmail.com>
2024-06-13 14:52:45 +0700

    Further script fixes

--- firemodescript.lua
+++ firemodescript.lua
@@ -1,3 +1,5 @@
+local Weapon = LocalPlayer():GetActiveWeapon()
+
 local MWBaseFiremodes = {
     ["AUTOMATIC"] = "Full-Auto",
 	["FULL AUTO"] = "Full-Auto",
@@ -20,8 +22,77 @@ local VanillaAutomatics = {
 	["weapon_egon"] = true
 }
 
+function IsInputBound(bind) -- Renamed ARC9 function. Don't wanna cause conflicts.
+    local key = input.LookupBinding(bind)
+
+    if !key then
+        return false
+    else
+        return true
+    end
+end
+
+
+function GetCWFiremodeName()
+	if Weapon:GetInUBGL() then return end
+	--AltFiremodeText = Weapon:GetBuff_Override("UBGL_PrintName") and Weapon:GetBuff_Override("UBGL_PrintName") or ArcCW.GetTranslation("fcg.ubgl" .. abbrev)
+
+	if Weapon:GetBuff_Hook("Hook_FiremodeName") then return Weapon:GetBuff_Hook("Hook_FiremodeName") end
+
+	--local abbrev = GetConVar("arccw_hud_fcgabbrev"):GetBool() and ".abbrev" or ""
+
+	--if Weapon:GetInUBGL() then
+	--	ActivePrimaryFire = false
+	--end
+
+	local fm = Weapon:GetCurrentFiremode()
+
+	if fm.PrintName then
+		local phrase = ArcCW.GetPhraseFromString(fm.PrintName)
+		return phrase and ArcCW.GetTranslation(phrase) or ArcCW.TryTranslation(fm.PrintName)
+	end
+
+	local mode = fm.Mode
+	if mode == 0 then return "Safety" end
+	if mode == 1 then return "Semi-Auto" end
+	if mode >= 2 then return "Full-Auto" end
+	if mode < 0 then return tostring(-mode) .. "-Burst" end
+end
+
+function GetA9FiremodeName()
+	AltFiremodeText = Weapon:GetProcessedValue("UBGLFiremodeName")
+
+	if Weapon:GetUBGL() then
+		ActivePrimaryFire = false
+	else
+	end
+
+	local arc9_mode = Weapon:GetCurrentFiremodeTable()
+	local FiremodeText = "UNKNOWN"
+
+	if arc9_mode.PrintName then
+		FiremodeText = arc9_mode.PrintName
+	else
+		if arc9_mode.Mode == 1 then
+			FiremodeText = "Semi-Auto"
+		elseif arc9_mode.Mode == 0 then
+			FiremodeText = "Safety"
+		elseif arc9_mode.Mode < 0 then
+			FiremodeText = "Full-Auto"
+		elseif arc9_mode.Mode > 1 then
+			FiremodeText = tostring(arc9_mode.Mode) .. "-Burst"
+		end
+	end
+
+	if Weapon:GetSafe() then
+		FiremodeText = "Safety"
+	end
+
+	return FiremodeText
+end
+
 function GetCurrentWeaponFiremode()
-	local Weapon = LocalPlayer():GetActiveWeapon()
+	Weapon = LocalPlayer():GetActiveWeapon()
 
 	if !IsValid(Weapon) then return end
 
@@ -53,14 +124,14 @@ function GetCurrentWeaponFiremode()
 	if isarc9 then -- Biggest blunder ever: forgor to change a9 to isarc9. bruh.
 		local arc9_mode = Weapon:GetCurrentFiremodeTable()
 
-		FiremodeText = GetA9FiremodeName()
+		FiremodeString = GetA9FiremodeName()
 
 		if Weapon:GetUBGL() then
 			--arc9_mode = {
 			--	Mode = Weapon:GetCurrentFiremode(),
 			--	PrintName = Weapon:GetProcessedValue("UBGLFiremodeName")
 			--}
-			--FiremodeText = arc9_mode.PrintName
+			--FiremodeString = arc9_mode.PrintName
 			wepmultifire = false
 			ActivePrimaryFire = false
 		end
@@ -78,76 +149,76 @@ function GetCurrentWeaponFiremode()
 	elseif Weapon.ArcCW then
 		local arccw_mode = Weapon:GetCurrentFiremode()
 
-		AltFiremodeText = Weapon:GetBuff_Override("UBGL_PrintName") and Weapon:GetBuff_Override("UBGL_PrintName") or ArcCW.GetTranslation("fcg.ubgl")
+		AltFiremodeString = Weapon:GetBuff_Override("UBGL_PrintName") and Weapon:GetBuff_Override("UBGL_PrintName") or ArcCW.GetTranslation("fcg.ubgl")
 
 		if Weapon:GetInUBGL() then
 			ActivePrimaryFire = false
 		else
-			FiremodeText = GetCWFiremodeName()
+			FiremodeString = GetCWFiremodeName()
 		end
 
-		if string.match(FiremodeText, "-round burst") then
-			string.Replace(FiremodeText, "-round burst", "-BURST")
-		elseif string.match(FiremodeText, "-ROUND BURST") then -- bruh case sensitivity is a thing
-			string.Replace(FiremodeText, "-ROUND BURST", "-BURST")
+		if string.match(FiremodeString, "-round burst") then
+			string.Replace(FiremodeString, "-round burst", "-BURST")
+		elseif string.match(FiremodeString, "-ROUND BURST") then -- bruh case sensitivity is a thing
+			string.Replace(FiremodeString, "-ROUND BURST", "-BURST")
 		end
 
-		FiremodeText = string.upper(FiremodeText)
+		FiremodeString = string.upper(FiremodeString)
 
 		if Weapon:GetMalfunctionJam() then
 			WeaponJammed = true
 		end
 	elseif ismgbase then
 		-- FIXME: Fix Safety detection for the dev build of MWBase and make it also work for the public build.
-		FiremodeText = string.upper(Weapon.Firemodes[Weapon:GetFiremode()].Name) -- Do we need two complicated tables for this?
+		FiremodeString = string.upper(Weapon.Firemodes[Weapon:GetFiremode()].Name) -- Do we need two complicated tables for this?
 		for k,v in pairs(MWBaseFiremodes) do
-			if k == FiremodeText then
-				FiremodeText = v
+			if k == FiremodeString then
+				FiremodeString = v
 			end
 		end
 	elseif istfabase then
-		FiremodeText = Weapon:GetFireModeName() -- Do we need two complicated tables for this?
+		FiremodeString = Weapon:GetFireModeName() -- Do we need two complicated tables for this?
 		for k,v in pairs(TFAFiremodes) do
-			if k == FiremodeText then
-				FiremodeText = v
+			if k == FiremodeString then
+				FiremodeString = v
 			end
 		end
 		-- It's a miracle how all of these bases don't conflict regarding their GetFiremode() or equivalent function.
 	elseif Weapon:IsScripted() then
 		if !Weapon.Primary.Automatic then
-			FiremodeText = "Semi-Auto"
+			FiremodeString = "Semi-Auto"
 		end
 
 		if Weapon.ThreeRoundBurst then
-			FiremodeText = "3-Burst"
+			FiremodeString = "3-Burst"
 		end
 
 		if Weapon.TwoRoundBurst then
-			FiremodeText = "2-Burst"
+			FiremodeString = "2-Burst"
 		end
 
 		if Weapon.GetSafe then
 			if Weapon:GetSafe() then
-				FiremodeText = "Safety"
+				FiremodeString = "Safety"
 			end
 		end
 
 		if isfunction(Weapon.Safe) then
 			if Weapon:Safe() then
-				FiremodeText = "Safety"
+				FiremodeString = "Safety"
 			end
 		end
 
 		if isfunction(Weapon.Safety) then
 			if Weapon:Safety() then
-				FiremodeText = "Safety"
+				FiremodeString = "Safety"
 			end
 		end
 	elseif !VanillaAutomatics[Weapon:GetClass()] then
-		FiremodeText = "Semi-Auto"
+		FiremodeString = "Semi-Auto"
 	end
 
-	if !isarc9 and !Weapon.ArcCW then AltFiremodeText = "Altfire" end
+	if !isarc9 and !Weapon.ArcCW then AltFiremodeString = "Altfire" end
 
 	if isarc9 and !IsInputBound("+arc9_ubgl") then
 		ubglkey = "[" .. usekey .."]+" .. "[" .. attack2 .. "]"
